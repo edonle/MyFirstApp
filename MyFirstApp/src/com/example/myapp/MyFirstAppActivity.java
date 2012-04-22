b6f83510654c6eac5af0a1798665ea352e4a0767
Edson <edonle@gmail.com>
2012-04-21 21:43:54 -0300

    Testando o controle de GPS

--- MyFirstApp/src/com/example/myapp/MyFirstAppActivity.java
+++ MyFirstApp/src/com/example/myapp/MyFirstAppActivity.java
@@ -1,28 +1,64 @@
 package com.example.myapp;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.EditText;
 
 public class MyFirstAppActivity extends Activity {
-    
+
 	public final static String EXTRA_MESSAGE = "com.example.myapp.MESSAGE";
-	
+
 	/** Called when the activity is first created. */
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
-    
-    /** Called when the user selects the Send button */
-    public void sendMessage(View view) {
-    	Intent intent = new Intent(this, DisplayMessageActivity.class);
-    	EditText editText = (EditText) findViewById(R.id.edit_message);
-    	String message = editText.getText().toString();
-    	intent.putExtra(EXTRA_MESSAGE, message);
-    	startActivity(intent);
-    }
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+	}
+
+	/** Called when the user selects the Send button */
+	public void sendMessage(View view) {
+
+		LocationManager locManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+
+		if (!locManager.isProviderEnabled(LocationManager.GPS_PROVIDER)) {
+			createGpsDisabledAlert();
+		} else {
+			Intent intent = new Intent(this, DisplayMessageActivity.class);
+			EditText editText = (EditText) findViewById(R.id.edit_message);
+			String message = editText.getText().toString();
+			intent.putExtra(EXTRA_MESSAGE, message);
+			startActivity(intent);
+		}
+	}
+
+	private void createGpsDisabledAlert() {
+		AlertDialog.Builder builder = new AlertDialog.Builder(this);
+		builder.setMessage("Your GPS is disabled! Would you like to enable it?")
+				.setCancelable(false)
+				.setPositiveButton("Enable GPS",
+						new DialogInterface.OnClickListener() {
+							public void onClick(DialogInterface dialog, int id) {
+								showGpsOptions();
+							}
+						});
+		builder.setNegativeButton("Do nothing",
+				new DialogInterface.OnClickListener() {
+					public void onClick(DialogInterface dialog, int id) {
+						dialog.cancel();
+					}
+				});
+		AlertDialog alert = builder.create();
+		alert.show();
+	}
+
+	private void showGpsOptions() {
+		Intent gpsOptionsIntent = new Intent(
+				android.provider.Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+		startActivity(gpsOptionsIntent);
+	}
 }
